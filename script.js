15bea0c057e5289973de2c0b84304d5bf95721f1
Kristoffer Nolgren <kirstoffer@nolgren.se>
2017-06-21 14:38:37 +0200

    ,

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $( document ).ready(function() {
 	var update = function(i){
 		var checks = $('.check').toArray();
 		var string = $('.hello').val();
+		var mode = $('.slider').val();
+
 		string += '\n\n\n';
 		checks.forEach(function(check){
 			if($(check).is(':checked')){
-				string += $(check).data('nice');
+				string += $(check).data('mode'+mode);
 				string += '\n\n';
 			}
 		});
@@ -16,8 +18,11 @@ $( document ).ready(function() {
 		string += $('.name').val();
 
 		$('textarea').val(string);
-		console.log(string);
 	};
 	$('.input').on('change keydown', update);
+	$('.enable').on('click', function(){
+		$('textarea').removeAttr('disabled');
+		$('.inputGroup').hide();
+	})
 
 });
